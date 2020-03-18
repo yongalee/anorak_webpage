10d046337ecc2fc83ddda76ef202982191b6eb46
TJ <godori16@gmail.com>
2020-03-19 01:06:58 +0900

    Homepage main template is added

--- webpages_project/settings.py
+++ webpages_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'blyp_*#(s%42rnvgupw0-oqa_z0oaocjm5-f7)mfqc5g^k4byj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
